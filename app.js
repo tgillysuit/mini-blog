00aecc08d175714b5fa636c731365cbb0a29b820
Tyler Gilmore <tyler.j.gilmoree@gmail.com>
2024-12-02 10:18:28 -0800

    Fixed, changed, and cleaned up some of the code in app.js. Fixed a typo for not sending out the timestamp on entries. Also removed the posts route because there was no purpose.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 const pool = mariadb.createPool({
     host: 'localhost',
     user: 'root',
-    password: '1234', // Change your password for your database here.
+    password: '****',
     database: 'miniblog'
 });
 
@@ -15,127 +15,94 @@ async function connect() {
         console.log('Connected to the database');
         return conn;
     } catch (err) {
-        console.log('Error connecting to the database ' + err);
+        console.error('Error connecting to the database', err);
+        throw err;
     }
-};
+}
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.static('views'));
-
 app.set('view engine', 'ejs');
 
-// The Home page, where the user enters in their post
+// Home page
 app.get('/', (req, res) => {
-    console.log("Homepage - server!");
-    
     res.render('home');
 });
 
+// Submit a new post
 app.post('/submit', async (req, res) => {
-    //Vars
     const data = req.body;
-    console.log(data);
-    const conn = await connect();
     let errors = [];
-    //Functions
-    let validationBools = {
-        title: true, content: true, author: true,
-        check: function(errors) {
-            if(this.title === true && this.content === true) {
+    const conn = await connect();
+
+    const validationBools = {
+        title: true,
+        content: true,
+        author: true,
+        check(errors) {
+            if (this.title && this.content) {
                 return true;
             }
-            else {
-                if(this.title === false) {
-                    errors.push("Title must be a least six characters.");
-                }
-                if(this.content === false) {
-                    errors.push("Content cannot be empty.");
-                }
-                return false;
-            }
+            if (!this.title) errors.push("Title must be at least six characters.");
+            if (!this.content) errors.push("Content cannot be empty.");
+            return false;
         }
+    };
 
+    function validationChecks(data, validationObject) {
+        if (data.title.trim().length <= 5) validationObject.title = false;
+        if (data.content.trim().length === 0) validationObject.content = false;
+        if (data.author.trim() === "") data.author = null;
     }
 
-    let validationChecks = function(data, validationObject) {
-        
-        if(data.title.trim().length <= 5) {
-            validationObject.title = false;
+    async function localInsert(connection) {
+        try {
+            await connection.query(
+                `INSERT INTO posts (author, title, content)
+                VALUES (?, ?, ?)`,
+                [data.author, data.title, data.content]
+            );
+        } catch (err) {
+            console.error('Error inserting into database:', err);
+            throw err;
         }
-        if(data.content.length <= 0) {
-            validationObject.content = false;
-        }
-        if(data.author.trim() == "") {
-            validationObject.author = false;
-            data.author = null;
-        }
-
     }
 
-    let localInsert = async function(connection) {
-        /*
-        Eventually, this will need to REALLY be sanitized. 
-        We could whitelist operations and sanitize inputs on the server-side.
-        My encapsulation will allow for this.
-        */
-       console.log(data);
-       let date = await connection.query(`SELECT Now() AS time;`);
-       console.log(date);
-        await connection.query(
-        `INSERT INTO posts (author, title, content, timestamp)
-        VALUES ('${data.author}','${data.title}','${data.content}','${date[0].time}');
-        `);
-    }
+    try {
+        validationChecks(data, validationBools);
 
-    let updateDB = function(connection, data, validationObject, insertFunction) {
-        validationChecks(data, validationObject);
-        
-        if(validationObject.check(errors) === true) {
-            insertFunction(connection);
+        if (validationBools.check(errors)) {
+            await localInsert(conn);
             res.render('confirmation', { details: data, errors: [] });
+        } else {
+            res.render('home', { details: data, errors });
         }
-        else {
-            if(!validationObject.title) {
-                console.log("Invalid Title");
-            }
-            if(!validationObject.content) {
-                console.log("Invalid Content");
-            }
-            res.render('home', { details: data, errors: [] });
-        }
+    } catch (err) {
+        console.error('Error during post submission:', err);
+        res.status(500).send('Internal Server Error');
+    } finally {
+        if (conn) conn.release();
     }
-    //Code
-
-    updateDB(conn, data, validationBools, localInsert);
-
-
 });
 
-app.get('/entries' , async (req, res) => {
-    const conn =  await connect();
-    function pullFromDB(connection) {
-        return connection.query(
-            `SELECT * FROM posts ORDER BY 'timestamp' DESC;`
-        );
+// Display all entries
+app.get('/entries', async (req, res) => {
+    const conn = await connect();
+
+    try {
+        const posts = await conn.query(`SELECT * FROM posts ORDER BY created_at DESC;`);
+        res.render('entries', { data: posts });
+    } catch (err) {
+        console.error('Error fetching posts:', err);
+        res.status(500).send('Internal Server Error');
+    } finally {
+        if (conn) conn.release();
     }
-
-    let posts = await pullFromDB(conn);
-
-    console.log(posts)
-
-
-
-
-    res.render('entries', { data: posts });
 });
 
-app.get('/posts' , (req,res) => {
-    res.render('posts', { data: posts });
-});
-
+// Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
